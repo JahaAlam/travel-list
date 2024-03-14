5bb2ea0fc05c996ab1d66dc7b319c79c535b11f4
JahangirAlam <jahangiralamarif34@gmail.com>
2024-03-14 03:01:59 +0100

    TODO list UI design done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
 
-function App() {
+export default function App(){
+return (
+<div className="app">
+  <Logo />
+  <Form />
+  <PackingList />
+  <Stats />
+</div>
+
+)
+
+}
+
+
+function Logo(){
+  return <h1>🌴Far Away💼 </h1>
+}
+  
+
+function Form(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((num)=>(
+          <option value={num} key={num}>
+            {num}
+          </option>
+        
+        ))}
+        {/* <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+        <option value={4}>4</option> */}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  )
+}
+
+
+function PackingList(){
+  return (
+    <div className="list">   
+    <ul>
+      {initialItems.map((item) =>(
+      <Item item={item} key={item.id}/>
+      ))}
+    </ul>
+  </div>
   );
 }
 
-export default App;
+function Item({item}){
+  return <li>
+    <spam style={item.packed ? {textDecoration: "line-through"}:{}}>
+    {item.quantity} {item.description}
+    </spam>
+    <button>❌</button>
+  </li>
+
+}
+
+
+
+function Stats(){
+  return <footer className="stats">
+    <em>you have X items on your list, and you already x (x%)</em>
+  </footer>
+}
