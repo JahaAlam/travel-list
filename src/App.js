1c2570cc2f7809140cb4a586e0f8f475aaa50bfb
JahangirAlam <jahangiralamarif34@gmail.com>
2024-03-22 00:33:10 +0100

    state use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
@@ -24,10 +26,26 @@ function Logo(){
   
 
 function Form(){
-  return (
-    <form className="add-form">
+   const [description, setDescription] = useState("");
+   const [quantity, setQuantity] = useState(1);
+
+   function handleSubmit(e){
+    e.preventDefault();
+    if(!description) return;
+
+    const newItem = {
+      description,quantity,packed: false, id: Date.now()};
+      console.log(newItem);
+  
+      setDescription("");
+      setQuantity(1);
+
+   }
+    return (
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e)=> setQuantity
+        (Number(e.target.value))}>
         {Array.from({ length: 10 }, (_, i) => i + 1).map((num)=>(
           <option value={num} key={num}>
             {num}
@@ -39,7 +57,9 @@ function Form(){
         <option value={3}>3</option>
         <option value={4}>4</option> */}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input type="text" placeholder="Item..." value={description}
+       onChange={(e)=>setDescription(e.target.value)}
+        />
       <button>Add</button>
     </form>
   )
@@ -58,15 +78,17 @@ function PackingList(){
   );
 }
 
-function Item({item}){
-  return <li>
-    <spam style={item.packed ? {textDecoration: "line-through"}:{}}>
-    {item.quantity} {item.description}
-    </spam>
-    <button>❌</button>
-  </li>
-
+function Item({ item }) {
+  return (
+    <li>
+      <spam style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </spam>
+      <button>❌</button>
+    </li>
+  );
 }
+
 
 
 
